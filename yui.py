17babdbbda71cace4ce7882b80b49db0c2d9da74
mariobob <mcalts71@gmail.com>
2018-07-09 13:58:29 -0400

    forget eval

--- yui.py
+++ yui.py
@@ -145,51 +145,6 @@ async def help(ctx):
             await bot.say(s)
 
 
-@bot.command(pass_context = True, hidden=True, name='eval')
-async def _eval(ctx, *, body: str):
-    if ctx.message.author.id in ownerids:
-        env = {
-        'bot': bot,
-        'ctx': ctx,
-        'channel': ctx.message.channel,
-        'author': ctx.message.author,
-        'guild': ctx.message.server,
-        'message': ctx.message,
-    }
-
-    env.update(globals())
-  
-    body = cleanup_code(body)
-    stdout = io.StringIO()
-
-    to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
-
-    try:
-        exec(to_compile, env)
-    except Exception as e:
-        return await ctx.bot.say(f'```py\n{e.__class__.__name__}: {e}\n```')
-
-    func = env['func']
-    try:
-        with redirect_stdout(stdout):
-            ret = await func()
-    except Exception as e:
-        value = stdout.getvalue()
-        await ctx.bot.say(f'```py\n{value}{type(e).__name__}: {e}\n```')
-    else:
-        value = stdout.getvalue()
-        try:
-            await ctx.message.add_reaction('\u2705')
-        except:
-            pass
-
-        if ret is None:
-            if value:
-                await ctx.bot.say(f'```py\n{value}\n```')
-        else:
-            await ctx.bot.say(f'```py\n{value}{ret}\n```')
-    else: 
-         await ctx.bot.say("Sorry. You are not a developer.")
 
 
         
